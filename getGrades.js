bcaa6f0a28160cc9a20f938b133a9bc585c50d12
Branden Palmer <brandenpalmer08@gmail.com>
2024-06-29 09:53:45 +0700

    formatting

--- getGrades.js
+++ getGrades.js
@@ -1,63 +1,90 @@
 // Just nav the grades page hit f-12 and paste this into the dev console
 function removeZeroPointRows() {
-  const allPossibleGradeSpans = [...document.querySelectorAll(".student_assignment .assignment_score .tooltip .grade+span")]
-  const zeroPointSpans = allPossibleGradeSpans.filter(el => parseInt(el.textContent.replace("/", "")) === 0)
-  const zeroPointRows = zeroPointSpans.map(el => el.parentElement.parentElement.parentElement.parentElement)
-  zeroPointRows.forEach(row => row.remove())
+  const allPossibleGradeSpans = [
+    ...document.querySelectorAll(
+      ".student_assignment .assignment_score .tooltip .grade+span",
+    ),
+  ];
+  const zeroPointSpans = allPossibleGradeSpans.filter(
+    (el) => parseInt(el.textContent.replace("/", "")) === 0,
+  );
+  const zeroPointRows = zeroPointSpans.map(
+    (el) => el.parentElement.parentElement.parentElement.parentElement,
+  );
+  zeroPointRows.forEach((row) => row.remove());
 }
 function getGradedRows() {
   return getEditableRows().filter(rmRowsWithoutActualScore);
-
 }
 function getEditableRows() {
-  return [...document.getElementsByClassName("student_assignment editable")]
+  return [...document.getElementsByClassName("student_assignment editable")];
 }
 function rmRowsWithoutActualScore(row) {
-  return !Number.isNaN(getScore(row))
+  return !Number.isNaN(getScore(row));
 }
 function getScore(row) {
-  const grade = row.querySelector(".grade")
+  const grade = row.querySelector(".grade");
   if (grade.classList.contains("changed")) {
-    return parseFloat(grade.textContent.trim())
+    return parseFloat(grade.textContent.trim());
   }
-  return parseFloat(grade.lastChild.textContent.trim())
+  return parseFloat(grade.lastChild.textContent.trim());
 }
 function rowToAssignmentObject(row) {
   return {
     groupType: row.querySelector("th > .context").textContent,
     actualScore: getScore(row),
-    possibleScore: parseFloat(row.querySelector(" td.assignment_score div.score_holder span.tooltip >span.grade+span").innerText.replace(/^\//, "")),
-  }
+    possibleScore: parseFloat(
+      row
+        .querySelector(
+          " td.assignment_score div.score_holder span.tooltip >span.grade+span",
+        )
+        .innerText.replace(/^\//, ""),
+    ),
+  };
 }
 function getGroups() {
-  return [...document.querySelectorAll("div#assignments-not-weighted div table.summary tbody tr")].map(el => ({ group: el.querySelector("th").textContent, weight: parseInt(el.querySelector("td").textContent.replace("%", "")) })).filter((el) => el.group != "Total");
+  return [
+    ...document.querySelectorAll(
+      "div#assignments-not-weighted div table.summary tbody tr",
+    ),
+  ]
+    .map((el) => ({
+      group: el.querySelector("th").textContent,
+      weight: parseInt(el.querySelector("td").textContent.replace("%", "")),
+    }))
+    .filter((el) => el.group != "Total");
 }
 function getScale(groupType) {
-  return getGroups().find(el => el.group === groupType).weight;
+  return getGroups().find((el) => el.group === groupType).weight;
 }
 function getAverage(assignments) {
-  const groups = getGroups()
-  const averages = groups.map(el => weightedAverageForGroup(assignments, el.group))
-  return averages.reduce(toTotal, 0)
+  const groups = getGroups();
+  const averages = groups.map((el) =>
+    weightedAverageForGroup(assignments, el.group),
+  );
+  return averages.reduce(toTotal, 0);
   function toTotal(total, current) {
-    return total += current
+    return (total += current);
   }
 }
 function weightedAverageForGroup(assignments, group) {
-  const filteredGroup = assignments.filter(el => el.groupType === group)
-  const total = filteredGroup.reduce((result, curr) => result += curr.actualScore / curr.possibleScore, 0)
+  const filteredGroup = assignments.filter((el) => el.groupType === group);
+  const total = filteredGroup.reduce(
+    (result, curr) => (result += curr.actualScore / curr.possibleScore),
+    0,
+  );
   // if category has no entries set the average to one for max points
   const average = filteredGroup.length === 0 ? 1 : total / filteredGroup.length;
-  const weightedAverage = average * getScale(group)
-  return weightedAverage
+  const weightedAverage = average * getScale(group);
+  return weightedAverage;
 }
 function replaceDisabledMsg() {
   const msgBox = document.querySelector("#student-grades-final");
-  msgBox.innerHTML = `<s>Calculation</s>"Estimation"🤔 of totals has been enabled\n`
-  const allAssignments = getGradedRows().map(rowToAssignmentObject)
+  msgBox.innerHTML = `<s>Calculation</s>"Estimation"🤔 of totals has been enabled\n`;
+  const allAssignments = getGradedRows().map(rowToAssignmentObject);
   const nonDroppedAssignments = getGradedRows()
     .filter((el) => !el.classList.contains("dropped"))
-    .map(rowToAssignmentObject)
+    .map(rowToAssignmentObject);
   msgBox.innerHTML += `<table class="summary">
     <thead>
     <tr>
@@ -66,12 +93,14 @@ function replaceDisabledMsg() {
     </tr>
     </thead>
     <tbody>
-${getGroups().map(({ group }) => {
-    return `<tr>
+${getGroups()
+      .map(({ group }) => {
+        return `<tr>
               <th scope="row">${group}</th>
               <td>${Math.floor(weightedAverageForGroup(allAssignments, group))}%</td>
-            </tr>`
-  }).join("")}
+            </tr>`;
+      })
+      .join("")}
         <tr>
           <th scope="row">All</th>
           <td>${Math.floor(getAverage(allAssignments))}%</td>
@@ -81,13 +110,13 @@ ${getGroups().map(({ group }) => {
           <td>${Math.floor(getAverage(nonDroppedAssignments))}%</td>
         </tr>
     </tbody>
-  </table>`
+  </table>`;
 }
 // recalc score when you click to edit score
 function addChangeListeners() {
-  getEditableRows().forEach((row) => row.onclick = replaceDisabledMsg)
+  getEditableRows().forEach((row) => (row.onclick = replaceDisabledMsg));
 }
-removeZeroPointRows()
-addChangeListeners()
-replaceDisabledMsg()
-console.log("Success now view screen where the disabled msg was.")
+removeZeroPointRows();
+addChangeListeners();
+replaceDisabledMsg();
+console.log("Success now view screen where the disabled msg was.");
